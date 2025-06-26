85af02b471a01982818328806c669874544c8764
dev-igss <devigsshgq@gmail.com>
2025-06-26 17:33:14 -0600

    Update dashboard.blade.php

--- code/code-dev/resources/views/admin/dashboard.blade.php
+++ code/code-dev/resources/views/admin/dashboard.blade.php
@@ -11,10 +11,48 @@
             </div>
 
             <div class="row mtop16">
-                
+
+                <div class="col-md-4">
+                    <div class="panel shadow">
+                        <div class="header">
+                            <h2 class="title"><i class="fas fa-users"></i><strong> Citas Agendadas</strong></h2>
+                        </div>
+                        <div class="inside">
+                            <div class="big_count">
+                                {{ $citas_d }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="panel shadow">
+                        <div class="header">
+                            <h2 class="title"><i class="fas fa-users"></i><strong> Citas en Atención</strong></h2>
+                        </div>
+                        <div class="inside">
+                            <div class="big_count">
+                                {{ $citas_d_aten }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="panel shadow">
+                        <div class="header">
+                            <h2 class="title"><i class="fas fa-users"></i><strong> Citas Ausentes</strong></h2>
+                        </div>
+                        <div class="inside">
+                            <div class="big_count">
+                                {{ $citas_d_aus }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
                 
-            </div>
+            </div>  
         @endif
     </div>
 
