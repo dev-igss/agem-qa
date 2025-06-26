e18f748b3214a39936893b9af6b81a7105e1f061
dev-igss <devigsshgq@gmail.com>
2025-06-26 17:37:43 -0600

    Update dashboard.blade.php

--- code/code-dev/resources/views/admin/dashboard.blade.php
+++ code/code-dev/resources/views/admin/dashboard.blade.php
@@ -3,57 +3,91 @@
 
 @section('content')
     <div class="container-fluid">
-        @if(kvfj(Auth::user()->permissions, 'dashboard_small_stats'))
-            <div class="panel shadow">
-                <div class="header">
-                    <h2 class="title"><i class="fas fa-chart-bar"></i> Estadísticas Rápidas</h2>
+
+        <div class="panel shadow">
+            <div class="header">
+                <h2 class="title"><i class="fas fa-chart-bar"></i> Estadísticas Rápidas</h2>
+            </div>
+        </div>
+
+        <div class="row mtop16">
+
+            <div class="col-md-4">
+                <div class="panel shadow">
+                    <div class="header">
+                        <h2 class="title"><i class="fas fa-users"></i><strong> Citas Agendadas</strong></h2>
+                    </div>
+                    <div class="inside">
+                        <div class="big_count">
+                            {{ $citas_d }}
+                        </div>
+                    </div>
                 </div>
             </div>
 
-            <div class="row mtop16">
-
-                <div class="col-md-4">
-                    <div class="panel shadow">
-                        <div class="header">
-                            <h2 class="title"><i class="fas fa-users"></i><strong> Citas Agendadas</strong></h2>
-                        </div>
-                        <div class="inside">
-                            <div class="big_count">
-                                {{ $citas_d }}
-                            </div>
+            <div class="col-md-4">
+                <div class="panel shadow">
+                    <div class="header">
+                        <h2 class="title"><i class="fas fa-users"></i><strong> Citas en Atención</strong></h2>
+                    </div>
+                    <div class="inside">
+                        <div class="big_count">
+                            {{ $citas_d_aten }}
                         </div>
                     </div>
                 </div>
+            </div>
 
-                <div class="col-md-4">
-                    <div class="panel shadow">
-                        <div class="header">
-                            <h2 class="title"><i class="fas fa-users"></i><strong> Citas en Atención</strong></h2>
-                        </div>
-                        <div class="inside">
-                            <div class="big_count">
-                                {{ $citas_d_aten }}
-                            </div>
+            <div class="col-md-4">
+                <div class="panel shadow">
+                    <div class="header">
+                        <h2 class="title"><i class="fas fa-users"></i><strong> Citas Ausentes</strong></h2>
+                    </div>
+                    <div class="inside">
+                        <div class="big_count">
+                            {{ $citas_d_aus }}
                         </div>
                     </div>
                 </div>
+            </div>
 
-                <div class="col-md-4">
-                    <div class="panel shadow">
-                        <div class="header">
-                            <h2 class="title"><i class="fas fa-users"></i><strong> Citas Ausentes</strong></h2>
-                        </div>
-                        <div class="inside">
-                            <div class="big_count">
-                                {{ $citas_d_aus }}
-                            </div>
-                        </div>
+            
+        </div>  
+
+        <div class="panel shadow">
+            <div class="header">
+                <h2 class="title"><i class="fas fa-chart-bar"></i> Noticias o Alertas del Sistema</h2>
+            </div>
+        </div>
+
+        <div class="row mtop16">
+
+            <div class="col-md-12">
+                <div class="panel shadow">
+                    <div class="header">
+                        <h2 class="title"><i class="fas fa-news"></i><strong> Citas Agendadas</strong></h2>
+                    </div>
+                    <div class="inside">
+                        <p>
+                            <ul>
+                                <li>
+                                    Para un mejor control de los modulos de citas, fueron separadas. 
+                                </li>
+                                <li>
+                                    El cita toma en cuenta los dias festivos, la jefatura del departamento de Radiologia sera la encargada de asignar los mismos.
+                                    Tomar en cuenta que las unicas citas validas para estos dias son de Hospitalización o Emergencia.
+                                </li>
+                            </ul>
+                        </p>
                     </div>
                 </div>
+            </div>
 
-                
-            </div>  
-        @endif
+
+
+            
+        </div>  
+
     </div>
 
 @endsection
