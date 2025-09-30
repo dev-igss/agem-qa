27163cd88db2e260fe9b98b7acd0acc43399af2b
dev-igss <devigsshgq@gmail.com>
2025-09-30 09:04:26 -0600

    Update informe.blade.php

--- code/code-dev/resources/views/admin/appointments/informe.blade.php
+++ code/code-dev/resources/views/admin/appointments/informe.blade.php
@@ -22,7 +22,7 @@
 
     <body style="font-size: 14px; font-family: 'Roboto Slab', serif;">
         <div style="float: left; margin-top: -25px;" >
-            <img src="{{ url('/static/imagenes/Isotipo.png') }}" alt="" width="80" height="80"/>
+            <img src="{{ url('/img/Isotipo.png') }}" alt="" width="80" height="80"/>
         </div>
         <div style="text-align: center; margin-top: 0px; margin-left: -1000px;">
             <span><strong> INFORME AL PATRONO </strong> </span>
