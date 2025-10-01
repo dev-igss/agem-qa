61f1ba6bafeb905f75bfded1c8872d85838f1a7a
dev-igss <devigsshgq@gmail.com>
2025-10-01 08:59:39 -0600

    Update home.blade.php

--- code/code-dev/resources/views/admin/statistics/home.blade.php
+++ code/code-dev/resources/views/admin/statistics/home.blade.php
@@ -101,7 +101,7 @@
                                                 <option value="{{ $key }}">{{ $value }}</option>
                                             @endforeach
                                         </select>
-                                        <input type="number" class="form-control" value="{{\Carbon\Carbon::now()->format('Y')}}" name="month_mamo" min="2022" max="2099" step="1">
+                                        <input type="number" class="form-control" value="{{\Carbon\Carbon::now()->format('Y')}}" name="year_mamo" min="2022" max="2099" step="1">
                                         
                                     </div>
 
