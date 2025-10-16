519a0943f5762cc1fa56b30b06a82acbc4692cc2
dev-igss <devigsshgq@gmail.com>
2025-10-16 12:59:07 -0600

    Update web.php

--- code/code-dev/routes/web.php
+++ code/code-dev/routes/web.php
@@ -38,6 +38,7 @@ Route::prefix('admin')->group(function () {
 
     Route::get('/bitacoras', [BitacoraController::class, 'getBitacora'])->name('bitacoras');
     Route::post('/bitacora/busqueda', [BitacoraController::class, 'postSearch'])->name('bitacoras');
+    Route::post('/bitacora/busqueda/', [BitacoraController::class, 'postSearch'])->name('bitacoras');
 
     //Citas
     Route::get('/citas/rx', [AppointmentController::class, 'getAppointmentRx'])->name('appointment_rx');
